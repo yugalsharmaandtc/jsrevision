283fc6d08a63cc39338f9e03c071b629da579f73
yugalsharmaandtc <yugalsharmaandtc@gmail.com>
2025-06-02 01:47:55 +0530

    commit message: Update ch-6dom.js

--- ch-6dom.js
+++ ch-6dom.js
@@ -1,1 +1,39 @@
-console.log("Hello World");
+//  window Object
+// The window object is the global object in the browser that provides access to everything from global variables, functions, and timers to the DOM and browser properties.
+
+
+
+// DOM MANIPULATION
+
+/*
+=> What is DOM Manipulation?
+DOM Manipulation means using JavaScript to change the structure, content, or style of a webpage after it has loaded.
+
+DOM stands for Document Object Model — it represents the HTML page as a tree of nodes (elements, attributes, text, etc.).
+
+=>Why DOM Manipulation is Used:
+Change text or HTML dynamically
+
+Add/remove elements
+
+Handle user interaction (e.g., button clicks)
+
+Apply styles or animations
+
+
+=>Common Methods for DOM Manipulation:
+Method	                               Purpose
+document.getElementById()	           Selects element by ID
+document.getElementsByClassname()	   Selects elements by class name
+document.getElementsByTagName()	       Selects elements by tag name
+document.querySelector()	           Selects the first matching element
+element.innerHTML	                   Sets or gets HTML content
+element.textContent	                   Sets or gets plain text
+element.style	                       Applies inline CSS
+document.createElement()	           Creates a new HTML element
+parent.appendChild()	               Adds a child element
+element.remove()	                   Removes the element
+
+
+*/
+
